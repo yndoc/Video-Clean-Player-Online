1b6fcf4d2c71a208e2f677050b1b2dc2ea20f609
yndoc <hero52@126.com>
2014-04-27 22:58:31 +0800

    VCPOn
    根据kafan大神cinhoo油猴脚本改写而来！
    感谢 15536900大神修改的播放器及cinhoo大神的脚本

---
+++ background.js
@@ -0,0 +1,245 @@
+/*
+ * This file is part of ADkill Player Offline
+ * <http://bbs.kafan.cn/thread-1514537-1-1.html>,
+ * Copyright (C) yndoc xplsy 15536900
+ *
+ * ADkill Player Offline is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * GNU General Public License, see <http://www.gnu.org/licenses/>.
+ */
+
+var proxyflag = 0;	//proxy调试标记
+var cacheflag = false;	//用于确定是否需要清理缓存,注意由于隐身窗口的cookie与缓存都独立与普通窗口,因此使用API无法清理隐身窗口的缓存与cookie.
+//var xhr = new XMLHttpRequest();	
+
+//====================================Crossdomin Spoofer Test
+//pac script
+var pac = {
+  mode: "pac_script",
+  pacScript: {
+    data: "function FindProxyForURL(url, host) {\n" +
+    	"	var regexpr = /.*\\/crossdomain\\.xml/;\n" +	//使用过程中\\将被解析成\,所以在正常正则表达式中的\/需要改写成\\/
+    	"	if(regexpr.test(url)){\n " +
+    	"		return 'PROXY yk.pp.navi.youku.com:80';\n" +
+    	"	}\n" +
+    	"	return 'DIRECT';\n" +
+    	"}"
+  }
+};
+//Permission Check + Proxy Control
+function ProxyControl(pram) {
+	chrome.proxy.settings.get({incognito: false}, function(config){
+		//console.log(config.levelOfControl);
+		//console.log(config);
+		//console.log(pac);
+		switch(config.levelOfControl) {
+			case "controllable_by_this_extension":
+			// 可获得proxy控制权限，显示信息
+			console.log("Have Proxy Permission");
+			proxyflag = 1;
+			if(pram == "set"){
+				console.log("Setup Proxy");
+				chrome.proxy.settings.set({value: pac, scope: "regular"}, function(details) {});
+			}
+			break;
+			case "controlled_by_this_extension":
+			// 已控制proxy，显示信息
+			console.log("Already controlled");
+			proxyflag = 2;
+			if(pram == "unset"){
+				console.log("Release Proxy");
+				chrome.proxy.settings.clear({scope: "regular"});
+				FlushCache();
+			}
+			break;
+			default:
+			// 未获得proxy控制权限，显示信息
+			console.log("No Proxy Permission");
+			console.log("Skip Proxy Control");
+			proxyflag = 0;
+			break;
+		}
+	});
+}
+function FlushCache() {
+	if(cacheflag) {
+		chrome.browsingData.remove(
+			{},{
+			"cache": true,
+			"fileSystems": true,
+		},
+		function() {
+			console.log('Now flushing Cache!');
+		});
+	}
+}
+//Listeners
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+	for (var i = 0; i < proxylist.length; i++) {
+		if (proxylist[i].find.test(details.url) && proxylist[i].extra == "crossdomain") {
+			//console.log(details.url);
+			console.log('Crossdomin Spoofer Rule : ' + proxylist[i].name);
+			switch (proxylist[i].name) {
+				default:
+				//console.log("In Proxy Set");
+				ProxyControl("set");
+				break;
+			}
+		}
+	}
+	//return {cancel: false};
+},
+{urls: ["http://*/*", "https://*/*"]},
+["blocking"]);
+chrome.webRequest.onCompleted.addListener(function(details) {
+	for (var i = 0; i < proxylist.length; i++) {
+		if (proxylist[i].monitor.test(details.url) && proxylist[i].extra == "crossdomain") {
+			//console.log(details);
+			cacheflag = false;
+			cacheflag = details.fromCache;
+			console.log("Capture Moniter Url :" + details.url + " fromCache :" + details.fromCache + " ip :" + details.ip);
+			switch (proxylist[i].name) {
+				default:
+				console.log("Now Release Proxy ");
+				ProxyControl("unset");
+				break;
+			}
+			break;
+		}
+	}
+},
+{urls:  ["http://*/*", "https://*/*"]});
+//标签开启
+chrome.tabs.onCreated.addListener(function(tab) {
+	ProxyControl("unset");
+});
+///标签关闭
+chrome.tabs.onRemoved.addListener(function(tabId) {
+	ProxyControl("unset");
+});
+//====================================Headers Modifier Test
+chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
+	//console.log(details);
+	for (var i = 0; i < refererslist.length; i++) {
+		if (refererslist[i].find.test(details.url)) {
+			//console.log(details);
+			console.log('Referer Modifier Rule : ' + refererslist[i].name);
+			for (var j = 0; j < details.requestHeaders.length; ++j) {
+				if (details.requestHeaders[j].name === 'Referer') {
+				//console.log(details.requestHeaders[j]);
+					switch (refererslist[i].name) {
+						case "referer_youku":
+						if (/(youku|tudou)/i.test(details.requestHeaders[j].value)) {
+							console.log("Referer Modifier : No need to change");
+							break;
+						}
+						case "referer_iqiyi":
+						if (/qiyi\.com/i.test(details.requestHeaders[j].value)) {
+							console.log("Referer Modifier : No need to change");
+							break;
+						}
+						default:
+						console.log("Referer Modifier : Switch Default");
+						if (refererslist[i].extra === "remove"){
+							console.log('Referer Modifier Action : Remove');
+							details.requestHeaders.splice(j, 1);
+						} else {
+							console.log('Referer Modifier Action : Modify');
+							details.requestHeaders[j].value = refererslist[i].replace;
+						}
+						break;
+					}
+				//console.log(details.requestHeaders[j]);
+					break;
+				}
+				/*if (details.requestHeaders[i].name === 'User-Agent') {
+	//details.requestHeaders.splice(i, 1);
+			details.requestHeaders[i].value = "Mozilla/5.0 (LETVC1;iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/535.35 (KHTML, like Gecko)";
+					//console.log(details.requestHeaders[i]);
+				}*/
+			}
+		}
+	}
+	//Add Cache Controler
+/*	for (var i = 0; i < proxylist.length; i++){
+			if (proxylist[i].realurl.test(details.url)) {
+				console.log('Cache-Control Modifier');
+				for (var j = 0; j < details.requestHeaders.length; ++j) {
+					if (details.requestHeaders[j].name === 'Cache-Control') {
+						details.requestHeaders[j].value = "no-cache";
+				}
+				break;
+			}
+		}
+	}
+*/
+	return {requestHeaders: details.requestHeaders};
+},{urls: ["http://*/*", "https://*/*"]},
+["blocking", "requestHeaders"]);
+//====================================
+//Referer修改规则
+/*格式：
+	name:规则名称
+	find:匹配(正则)表达式
+	replace:替换(正则)表达式,注意此处有多种方式,可看后续说明并按需选择
+	extra:额外的属性,remove表示去除Referer参数
+*/
+var refererslist = [{
+		name: "referer_youku",
+		find: /f\.youku\.com/i,
+		replace: "http://player.youku.com/player.php",
+		extra: ""	//use "remove" is also acceptable
+	},{
+		name: "referer_56",
+		find: /\.56\.com/i,
+		replace: "",
+		extra: "remove"
+	},{
+		name: "referer_iqiyi",
+		find: /cache\.video\.qiyi\.com/i,
+		replace: "",
+		extra: "remove"
+	}
+	]
+//Crossdomain修改规则
+/*格式：
+	name:规则名称
+	find:匹配(正则)表达式,当出现匹配地址时,启动crossdomain代理修改
+	monitor:匹配(正则)表达式,当出现匹配地址时,释放crossdomain代理
+	extra:额外的属性,crossdomain表示启动修改
+*/
+var proxylist = [{
+		name: "crossdomain_youku",
+		find: /http:\/\/static\.youku\.com\/.*?q?(player|loader)(_[^.]+)?\.swf/i,	//播放器载入地址
+		monitor:/http:\/\/v\.youku\.com\/crossdomain.xml/i,	//youku tudou实际访问的均是这个地址
+		extra: "crossdomain"
+	},{
+		name: "crossdomain_youku_out",
+		find: /^http:\/\/player\.youku\.com\/player\.php\/(.*\/)?sid\/([\w=]+)\/v\.swf/i,
+		monitor:/http:\/\/v\.youku\.com\/crossdomain.xml/i,
+		extra: "crossdomain"
+	},{
+		name: "crossdomain_tudou",
+		find: /.*PortalPlayer[^\.]*\.swf/i,
+		monitor:/http:\/\/v\.youku\.com\/crossdomain.xml/i,
+		extra: "crossdomain"
+	},{
+		name: "crossdomain_tudou_olc",
+		find: /^http:\/\/js\.tudouui\.com\/.*olc[^\.]*\.swf/i,
+		monitor:/http:\/\/v\.youku\.com\/crossdomain.xml/i,
+		extra: "crossdomain"
+	},{
+		name: "crossdomain_tudou_sp",
+		find: /.*olc[^\.]*\.swf/i,
+		monitor:/http:\/\/v\.youku\.com\/crossdomain.xml/i,
+		extra: "crossdomain"
+	},{
+		name: "crossdomain_sohu",
+		find: /http:\/\/(tv\.sohu\.com\/upload\/swf\/.*\d+|.*\/test\/player)\/main\.swf/i,
+//		monitor:/.*skins\/s[\d]+\.swf/i,
+		monitor: /http:\/\/live\.tv\.sohu\.com\/crossdomain\.xml/i,
+		extra: "crossdomain"
+	}
+]
---
+++ script.js
@@ -0,0 +1,258 @@
+﻿(function() {
+    Function.prototype.bind = function() {
+        var fn = this, args = Array.prototype.slice.call(arguments), obj = args.shift();
+        return function() {
+            return fn.apply(obj, args.concat(Array.prototype.slice.call(arguments)));
+        };
+    };
+
+    function YoukuAntiAds() {}
+    YoukuAntiAds.prototype = {
+        _players: null,
+        _rules: null,
+        _done: null,
+        get players() {
+            if(!this._players) {
+                this._players = {
+、、=====================在线版地址开始=================================================
+//双虚线之前为在线地址，请自行需该服务器地址！如使用本地版请注释掉双虚线以上地址
+                    'youku_loader': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/loader.swf',
+                    'youku_player': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/player.swf',
+                    'ku6': 'http://haoutil.googlecode.com/svn/trunk/player/ku6.swf',
+                    'ku6_out': 'http://haoutil.googlecode.com/svn/trunk/player/ku6_out.swf',
+                    'iqiyi': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/iqiyi.swf',
+                    'iqiyi5': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/iqiyi5.swf',
+                    'iqiyi_out': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/iqiyi_out.swf',
+                    'pps': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/pps.swf',
+                    'tudou': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/tudou.swf',
+                    'letv': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/letv.swf',
+//已无效                    'letvviki':'http://code.taobao.org/svn/noadsplayer/trunk/Player/letvviki-YN.swf',
+                    'sohu':'http://code.taobao.org/svn/noadsplayer/trunk/Player/sohu.swf',
+//======================在线版地址结束======================================================================================
+ //双虚线之间的地址不能换为本地地址，否则外联出错，请尽量更改为自己的服务器地址！
+                    'letv_o': 'http://code.taobao.org/svn/noadsplayer/trunk/Player/letv.swf',
+		    'letv_c': 'http://code.taobao.org/svn/noadsplayer/trunk/Player/letv0225.swf',
+                    'tudou_olc': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/olc_8.swf',
+                    'tudou_sp': 'http://haoutil.googlecode.com/svn/trunk/player/testmod/sp.swf',
+					'letvpccs': 'http://www.letv.com/zt/cmsapi/playerapi/pccs_sdk_$1.xml',
+//=======================本地版地址开始=====================================================================================
+//以下为本地版地址默认已使用（/* */）注释掉！
+/*                    'youku_loader': chrome.extension.getURL('swf/loader.swf'),
+                    'youku_player': chrome.extension.getURL('swf/player.swf'),
+                    'ku6': chrome.extension.getURL('swf/ku6.swf'),
+                    'ku6_out': chrome.extension.getURL('swf/ku6_out.swf'),
+                    'iqiyi': chrome.extension.getURL('swf/iqiyi.swf'),
+                    'iqiyi5': chrome.extension.getURL('swf/iqiyi5.swf'),
+                    'iqiyi_out': chrome.extension.getURL('swf/iqiyi_out.swf'),
+                    'pps': chrome.extension.getURL('swf/pps.swf'),
+                    'tudou': chrome.extension.getURL('swf/tudou.swf'),
+                    'tudou_olc': chrome.extension.getURL('swf/olc.swf'),
+                    'tudou_sp': chrome.extension.getURL('swf/sp.swf'),
+                    'letv': chrome.extension.getURL('swf/letv.swf'),  
+//已无效                    'letvviki': chrome.extension.getURL('swf/letvviki.swf'),  
+                    'sohu': chrome.extension.getURL('swf/sohu.swf'),
+//========================本地版地址结束========================================================
+*/
+               };
+            }
+            return this._players;
+        },
+        get rules() {
+            if(!this._rules) {
+                this._rules = {
+                    'youku_loader': {
+                        'find': /^http:\/\/static\.youku\.com(\/v[\d\.]+)?\/v\/swf\/loader\.swf/i,
+                        'replace': this.players['youku_loader']
+                    },
+                    'youku_player': {
+                        'find': /^http:\/\/static\.youku\.com(\/v[\d\.]+)?\/v\/swf\/q?player[^\.]*\.swf(\?.*)?/i,
+                        'replace': this.players['youku_loader'] + '$2'
+                    },
+                    'youku_out': {
+                        'find': /^http:\/\/player\.youku\.com\/player\.php\/.*sid\/([\w=]+).*(\/v)?\.swf.*/i,
+                        'replace': this.players['youku_loader'] + '?showAd=0&VideoIDS=$1'
+                    },
+                    'ku6': {
+                        'find': /^http:\/\/player\.ku6cdn\.com\/default\/.*\/\d+\/player\.swf/i,
+                        'replace': this.players['ku6']
+                    },
+                    'ku6_out': {
+                        'find': /^http:\/\/player\.ku6\.com\/(inside|refer)\/([^\/]+)\/v\.swf.*/i,
+                        'replace': this.players['ku6_out'] + '?vid=$2'
+                    },
+                    'iqiyi': {
+                        'find': /^http:\/\/www\.iqiyi\.com\/player\/\d+\/player\.swf/i,
+                        'replace': this.players['iqiyi']
+                    },
+                    'iqiyi_out': {
+                        'find': /^http:\/\/(player|dispatcher)\.video\.i?qiyi\.com\/(.*[\?&]vid=)?([^\/&]+).*/i,
+                        'replace': this.players['iqiyi_out'] + '?vid=$3'
+                    },
+                    'pps': {
+                        'find': /^http:\/\/www\.iqiyi\.com\/player\/cupid\/.*\/pps[\w]+.swf/i,
+                        'replace': this.players['pps']
+                    },
+                    'tudou': {
+                        'find': /^http:\/\/js\.tudouui\.com\/.*player[^\.]*\.swf/i,
+                        'replace': this.players['tudou']
+                    },
+                    'tudou_out': {
+                        'find': /^http:\/\/www\.tudou\.com\/.*(\/v\.swf)?/i,
+                        //'find': /^http:\/\/www\.tudou\.com\/.*\/v\.swf/i,
+                        'replace': this.players['tudou_olc'] + '?tvcCode=-1&swfPath=' + this.players['tudou_sp']
+                    },
+/*                    'letvcore': {
+                        'find': /^http:\/\/.*letv[\w]*\.com\/.*\/KLetvPlayer\.swf/i,
+                        'replace':  this.players['letvcore']
+                    },
+                    'letvviki': {
+                        'find': /^http:\/\/.*letv[\w]*\.com\/.*\/V[^\.]*\.swf/i,
+                        'replace': this.players['letvviki']
+                    },
+*/
+                    'letv': {
+                        'find': /^http:\/\/.*letv[\w]*\.com\/.*\/(?!(Live|seed))((S[\w]{2,3})?[\w]{4}|swf)Player[^\.]*\.swf/i,
+                        'replace': this.players['letv']
+                    },
+                    'letvpccs': {
+                        'find': /http:\/\/www.letv.com\/zt\/cmsapi\/playerapi\/pccs.*_(\d+)\.xml/i,
+                        'replace': this.players['letvpccs']
+                    },
+
+                    'letv_c': {
+                        'find': /^http:\/\/.*(letv[\w]*|dwstatic)\.com\/.*(cloud|vpp)\.swf/i,
+                        'replace': this.players['letv_c']
+                    },
+                    'letv_out': {
+                        'find': /^http:\/\/.*\.letvimg\.com\/.*\/(letvbili|lbplayer|letv-wrapper)\.swf/i,
+                        'replace': this.players['letv_o']
+                    },
+ /*                   'pplive': {
+                        'find': /^http:\/\/player\.pplive\.cn\/ikan\/.*\/player4player2\.swf/i,
+                        'replace': this.players['pplive']
+                    },
+                    'pplive_live': {
+                        'find': /^http:\/\/player\.pplive\.cn\/live\/.*\/player4live2\.swf/i,
+                        'replace': this.players['pplive_live']
+                    },
+ */
+                    'sohu': {
+                        'find': /^http:\/\/(tv\.sohu\.com\/upload\/swf\/.*\d+|.*\/test\/player)\/(main|playershell)\.swf/i,
+                        'replace': this.players['sohu']
+		    }
+                }
+            }
+            return this._rules;
+        },
+        get done() {
+            if(!this._done) {
+                this._done = new Array();
+            }
+            return this._done;
+        },
+        initPreHandlers: function() {
+            this.rules['iqiyi']['preHandle'] = function(elem, find, replace, player) {
+                if(document.querySelector('span[data-flashplayerparam-flashurl]')) {
+                    replace = this.players['iqiyi5'];
+                }
+                this.reallyReplace.bind(this, elem, find, replace)();
+            }
+            this.rules['iqiyi_out']['preHandle'] = function(elem, find, replace, player) {
+                var match = player.match(/(autoplay)=[^&]+/ig);
+                if(match) {
+                    replace += '&' + match.join('&');
+                }
+                this.reallyReplace.bind(this, elem, find, replace)();
+            }
+            this.rules['tudou_out']['preHandle'] = function(elem, find, replace, player) {
+                var fn = this;
+                var isFx = /firefox/i.test(navigator.userAgent);
+                GM_xmlhttpRequest({
+                    method: isFx ? 'HEAD' : 'GET',
+                    //url: isFx ? player : 'http://lovejiani.cdn.duapp.com/kafan/tfetcher?turl=' + encodeURIComponent(player + '/v.swf'),
+                    url: isFx ? player : 'https://query.yahooapis.com/v1/public/yql?format=json&q=' + encodeURIComponent('use"https://haoutil.googlecode.com/svn/trunk/firefox/tudou_redirect.yql.xml" as tudou; select * from tudou where url="' + player + '" and referer="' + window.location.href + '"'),
+                    onload: function(response) {
+                        var finalUrl = (isFx ? response.finalUrl : response.responseText);
+                        var match = finalUrl.match(/(iid|youkuid|resourceid|autoplay|snap_pic)=[^&]+/ig);
+                        if(match && !/error/i.test(finalUrl)) {
+                            replace += '&' + match.join('&');
+                            fn.reallyReplace.bind(fn, elem, find, replace)();
+                        }
+                    }
+                });
+            }
+        },
+        addAnimations: function() {
+            var style = document.createElement('style');
+            style.type = 'text/css';
+            style.innerHTML = 'object,embed{\
+-webkit-animation-duration:.001s;-webkit-animation-name:playerInserted;\
+-ms-animation-duration:.001s;-ms-animation-name:playerInserted;\
+-o-animation-duration:.001s;-o-animation-name:playerInserted;\
+animation-duration:.001s;animation-name:playerInserted;}\
+@-webkit-keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}\
+@-ms-keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}\
+@-o-keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}\
+@keyframes playerInserted{from{opacity:0.99;}to{opacity:1;}}';
+            document.getElementsByTagName('head')[0].appendChild(style);
+        },
+        animationsHandler: function(e) {
+            if(e.animationName === 'playerInserted') {
+                this.replace(e.target);
+            }
+        },
+        replace: function(elem) {
+            if(this.done.indexOf(elem) != -1) return;
+            this.done.push(elem);
+
+            var player = elem.data || elem.src;
+            if(!player) return;
+
+            var i, find, replace, isReplacing = false;
+            for(i in this.rules) {
+                find = this.rules[i]['find'];
+                if(find.test(player)) {
+                    replace = this.rules[i]['replace'];
+                    if('function' === typeof this.rules[i]['preHandle']) {
+                        isReplacing = true;
+                        this.rules[i]['preHandle'].bind(this, elem, find, replace, player)();
+                    }
+                    if(!isReplacing) {
+                        this.reallyReplace.bind(this, elem, find, replace)();
+                    }
+                    break;
+                }
+            }
+        },
+        reallyReplace: function(elem, find, replace) {
+            elem.data && (elem.data = elem.data.replace(find, replace)) || elem.src && ((elem.src = elem.src.replace(find, replace)) && (elem.style.display = 'block'));
+            this.reloadPlugin(elem);
+        },
+        reloadPlugin: function(elem) {
+            var nextSibling = elem.nextSibling;
+            var parentNode = elem.parentNode;
+            parentNode.removeChild(elem);
+            var newElem = elem.cloneNode(true);
+            this.done.push(newElem);
+            if(nextSibling) {
+                parentNode.insertBefore(newElem, nextSibling);
+            } else {
+                parentNode.appendChild(newElem);
+            }
+        },
+        init: function() {
+            this.initPreHandlers();
+
+            var handler = this.animationsHandler.bind(this);
+
+            document.body.addEventListener('webkitAnimationStart', handler, false);
+            document.body.addEventListener('msAnimationStart', handler, false);
+            document.body.addEventListener('oAnimationStart', handler, false);
+            document.body.addEventListener('animationstart', handler, false);
+
+            this.addAnimations();
+        }
+    };
+
+    new YoukuAntiAds().init();
+})();
